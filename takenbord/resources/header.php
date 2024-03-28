cffa02b46d93e8a616fe6f25f1828a2461d2ad70
Renssus <74822300+Renssus@users.noreply.github.com>
2024-03-28 13:03:43 +0100

    taskBar

---
+++ takenbord/resources/header.php
@@ -0,0 +1,11 @@
+<?php require_once 'configs/config.php'; ?>
+
+<header>
+    <div class="container">
+        <nav class="d-flex align-items-center">
+            <h1 class="me-4">TaakBeheer</h1>
+            <a class="me-4" href="<?php echo $base_url; ?>/index.php">Home</a>
+            <a class="me-4" href="<?php echo $base_url; ?>/Create.php">Create</a>
+        </nav>
+    </div>
+</header>
